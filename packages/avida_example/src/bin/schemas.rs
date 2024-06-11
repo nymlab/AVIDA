151f5fcbce38ed28ae3d8d8f3ba820e0fc2f68c6
lampkin-diet <lampkin.diet@gmail.com>
2024-06-11 23:26:51 +0200

    Commit current state

---
+++ packages/avida_example/src/bin/schemas.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use avida_example::contract::sv::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ContractExecMsg,
+        query: ContractQueryMsg,
+    }
+}
--- packages/avida_example/src/contract.rs
+++ packages/avida_example/src/contract.rs
@@ -1,21 +1,23 @@
+use avida_common::traits::avida_verifier_trait::sv::AvidaVerifierTraitExecMsg;
 use avida_common::types::InputRoutesRequirements;
-use cosmwasm_std::{entry_point, from_json, DepsMut, Env, Reply, Response, StdResult, SubMsg};
+use sylvia::cw_std::{to_json_binary, WasmMsg};
+use sylvia::cw_std::{entry_point, from_json, DepsMut, Env, Reply, Response, StdResult, SubMsg};
+use sylvia::types::ReplyCtx;
 use sylvia::{
     contract, entry_points, schemars,
     types::InstantiateCtx,
 };
-use cw_storage_plus::{Item};
+use cw_storage_plus::{Item, Map};
 use cw_utils::parse_reply_execute_data;
 
 use crate::error::ContractError;
-use crate::msg::{IntoCosmos, VerifyRequest};
 use crate::constants::{GIVE_ME_DRINK_ROUTE_ID, GIVE_ME_FOOD_ROUTE_ID, GIVE_ME_GASOLINE_ROUTE_ID, REGISTER_REQUIREMENT_REPLY_ID};
-use crate::msg::{ExecuteMsg, GiveMeSomeDrink, GiveMeSomeFood, GiveMeSomeGasoline, RegisterRequirement, RegisterRequest};
-use crate::state::PENDING_TRANSACTIONS;
+use crate::msg::{ExecuteMsg, GiveMeSomeDrink, GiveMeSomeFood, GiveMeSomeGasoline, RegisterRequest, RegisterRequirement, VerifyRequest};
 
 
 pub struct RestaurantContract <'a>{
     verifier: Item<'a, String>,
+    pending_transactions: Map<'a, u64, ExecuteMsg>
 }
 
 #[entry_points]
@@ -24,6 +26,7 @@ impl RestaurantContract <'_> {
     pub const fn new() -> Self {
         Self {
             verifier: Item::new("verifier"),
+            pending_transactions: Map::new("pending_transactions"),
         }
     }
 
@@ -59,152 +62,154 @@ impl RestaurantContract <'_> {
             }
         }
 
-        let register_msg: RegisterRequest = RegisterRequest{
-            app_addr: ctx.info.sender.to_string(),
+        let register_msg = AvidaVerifierTraitExecMsg::Register { 
+            app_addr: ctx.env.contract.address.to_string(), 
             route_criteria: vec![route_requirements]
         };
 
         let verifier_contract = self.verifier.load(ctx.deps.storage)?;
 
-        // let register_msg = SubMsg::reply_on_success(
-        //     register_msg.into_cosmos_msg(verifier_contract)?, 
-        //     REGISTER_REQUIREMENT_REPLY_ID
-        // );
+        let sub_msg = SubMsg::reply_on_success(
+            WasmMsg::Execute {
+                contract_addr: verifier_contract,
+                msg: to_json_binary(&register_msg)?,
+                funds: vec![],
+            }, 
+            REGISTER_REQUIREMENT_REPLY_ID
+        );
         
-        // Ok(Response::new().add_submessage(register_msg))
-
-        Ok(Response::new()
-            .add_message(register_msg.into_cosmos_msg(verifier_contract)?))
+        Ok(Response::new().add_submessage(sub_msg))
     }
 
-    // Ask for the portion
+    // // Ask for the portion
     #[sv::msg(exec)]
     pub fn give_me_some_drink(&self, ctx: sylvia::types::ExecCtx, msg: GiveMeSomeDrink) -> StdResult<Response> {
         // 1. Save the transaction
         // 2. Send the request to verifier
         // 3. Wait for the reply
-        PENDING_TRANSACTIONS.save(ctx.deps.storage, GIVE_ME_DRINK_ROUTE_ID, &ExecuteMsg::GiveMeSomeDrink(msg.clone()))?;
-        // self.pending_transactions.save(ctx.deps.storage, &(ctx.info.clone(), ExecuteMsg::GiveMeSomeDrink(msg.clone())))?;
+        self.pending_transactions.save(ctx.deps.storage, GIVE_ME_DRINK_ROUTE_ID, &ExecuteMsg::GiveMeSomeDrink(msg.clone()))?;
         let verifier_contract = self.verifier.load(ctx.deps.storage)?;
         
-        let verify_request = SubMsg::reply_on_success(
-            VerifyRequest{
-                presentation: msg.proof,
-                route_id: GIVE_ME_DRINK_ROUTE_ID,
-                app_addr: Some(ctx.info.sender.to_string())
-            }.into_cosmos_msg(verifier_contract)?,
+        let verify_request = AvidaVerifierTraitExecMsg::Verify {
+            presentation: msg.proof,
+            route_id: GIVE_ME_DRINK_ROUTE_ID,
+            app_addr: Some(ctx.env.contract.address.to_string()),
+        };
+        let sub_msg = SubMsg::reply_on_success(
+            WasmMsg::Execute {
+                contract_addr: verifier_contract,
+                msg: to_json_binary(&verify_request)?,
+                funds: vec![],
+            },
             GIVE_ME_DRINK_ROUTE_ID,
         );
         
-
         Ok(Response::new()
-            .add_submessage(verify_request)
+            .add_submessage(sub_msg)
         )
     }
 
-    // Ask for the portion
-    #[sv::msg(exec)]
-    pub fn give_me_some_food(&self, ctx: sylvia::types::ExecCtx, msg: GiveMeSomeFood) -> StdResult<Response> {
-        PENDING_TRANSACTIONS.save(ctx.deps.storage, GIVE_ME_FOOD_ROUTE_ID, &ExecuteMsg::GiveMeSomeFood(msg.clone()))?;
-        let verifier_contract = self.verifier.load(ctx.deps.storage)?;
-        let verify_request = SubMsg::reply_on_success( 
-            VerifyRequest{
-                presentation: msg.proof,
-                route_id: GIVE_ME_FOOD_ROUTE_ID,
-                app_addr: Some(ctx.info.sender.to_string())
-            }.into_cosmos_msg(verifier_contract)?,
-            GIVE_ME_FOOD_ROUTE_ID
-        );
+    // // Ask for the portion
+    // #[sv::msg(exec)]
+    // pub fn give_me_some_food(&self, ctx: sylvia::types::ExecCtx, msg: GiveMeSomeFood) -> StdResult<Response> {
+    //     self.pending_transactions.save(ctx.deps.storage, GIVE_ME_FOOD_ROUTE_ID, &ExecuteMsg::GiveMeSomeFood(msg.clone()))?;
+    //     let verifier_contract = self.verifier.load(ctx.deps.storage)?;
+    //     let verify_request = SubMsg::reply_on_success( 
+    //         VerifyRequest{
+    //             presentation: msg.proof,
+    //             route_id: GIVE_ME_FOOD_ROUTE_ID,
+    //             app_addr: Some(ctx.info.sender.to_string())
+    //         }.into_cosmos_msg(verifier_contract)?,
+    //         GIVE_ME_FOOD_ROUTE_ID
+    //     );
 
-        Ok(Response::new()
-            .add_submessage(verify_request)
-        )
+    //     Ok(Response::new()
+    //         .add_submessage(verify_request)
+    //     )
 
-    }
+    // }
 
-    // Ask for the portion
-    #[sv::msg(exec)]
-    pub fn give_me_some_gasoline(&self, ctx: sylvia::types::ExecCtx, msg: GiveMeSomeGasoline) -> StdResult<Response> {
-        PENDING_TRANSACTIONS.save(ctx.deps.storage, GIVE_ME_GASOLINE_ROUTE_ID, &ExecuteMsg::GiveMeSomeGasoline(msg.clone()))?;
-        let verifier_contract = self.verifier.load(ctx.deps.storage)?;
+    // // Ask for the portion
+    // #[sv::msg(exec)]
+    // pub fn give_me_some_gasoline(&self, ctx: sylvia::types::ExecCtx, msg: GiveMeSomeGasoline) -> StdResult<Response> {
+    //     self.pending_transactions.save(ctx.deps.storage, GIVE_ME_GASOLINE_ROUTE_ID, &ExecuteMsg::GiveMeSomeGasoline(msg.clone()))?;
+    //     let verifier_contract = self.verifier.load(ctx.deps.storage)?;
 
-        let verify_request = SubMsg::reply_on_success(
-            VerifyRequest{
-                presentation: msg.proof,
-                route_id: GIVE_ME_GASOLINE_ROUTE_ID,
-                app_addr: Some(ctx.info.sender.to_string())
-            }.into_cosmos_msg(verifier_contract)?,
-            GIVE_ME_GASOLINE_ROUTE_ID,
-        );
+    //     let verify_request = SubMsg::reply_on_success(
+    //         VerifyRequest{
+    //             presentation: msg.proof,
+    //             route_id: GIVE_ME_GASOLINE_ROUTE_ID,
+    //             app_addr: Some(ctx.info.sender.to_string())
+    //         }.into_cosmos_msg(verifier_contract)?,
+    //         GIVE_ME_GASOLINE_ROUTE_ID,
+    //     );
 
-        Ok(Response::new()
-            .add_submessage(verify_request)
-        )
-    }
-}
+    //     Ok(Response::new()
+    //         .add_submessage(verify_request)
+    //     )
+    // }
 
-#[entry_point]
-pub fn reply(deps: DepsMut, _env: Env, reply: Reply) -> Result<Response, ContractError> {
-    // 
-    match reply.id {
-        REGISTER_REQUIREMENT_REPLY_ID => {
-            let verification_result = parse_reply_execute_data(reply)?;
-            if verification_result.data.is_none() {
+    #[sv::msg(reply)]
+    fn reply(&self, ctx: ReplyCtx, reply: Reply) -> Result<Response, ContractError> {
+        match reply.id {
+            REGISTER_REQUIREMENT_REPLY_ID => {
+                match reply.result.into_result() {
+                    Err(_) => return Err(ContractError::VerificationProcessError),
+                    Ok(_) => return Ok(Response::new()),
+                }
+            }
+            GIVE_ME_DRINK_ROUTE_ID => {
+                let verification_result = parse_reply_execute_data(reply)?;
+                let verified: bool = from_json(&verification_result.data.unwrap())?;
+                let msg = self.pending_transactions.load(ctx.deps.storage, GIVE_ME_DRINK_ROUTE_ID)?;
+                if verified {
+                    match msg {
+                        ExecuteMsg::GiveMeSomeDrink(msg) => {
+                            return Ok(Response::new()
+                                .add_attribute("action", "give_me_some_drink")
+                                .add_attribute("Drink kind", msg.kind)
+                            )
+                        }
+                        _ => return Err(ContractError::VerificationProcessError)
+                    }
+                }
                 return Err(ContractError::VerificationProcessError)
             }
-            return Ok(Response::new())
-        }
-        GIVE_ME_DRINK_ROUTE_ID => {
-            let verification_result = parse_reply_execute_data(reply)?;
-            let verified: bool = from_json(&verification_result.data.unwrap())?;
-            let msg = PENDING_TRANSACTIONS.load(deps.storage, GIVE_ME_DRINK_ROUTE_ID)?;
-            if verified {
-                match msg {
-                    ExecuteMsg::GiveMeSomeDrink(msg) => {
-                        return Ok(Response::new()
-                            .add_attribute("action", "give_me_some_drink")
-                            .add_attribute("Drink kind", msg.kind)
-                        )
+            GIVE_ME_FOOD_ROUTE_ID => {
+                let verification_result = parse_reply_execute_data(reply)?;
+                let verified: bool = from_json(&verification_result.data.unwrap())?;
+                let msg = self.pending_transactions.load(ctx.deps.storage, GIVE_ME_FOOD_ROUTE_ID)?;
+                if verified {
+                    match msg {
+                        ExecuteMsg::GiveMeSomeFood(msg) => {
+                            return Ok(Response::new()
+                                .add_attribute("action", "give_me_some_food")
+                                .add_attribute("Food kind", msg.kind)
+                            )
+                        }
+                        _ => return Err(ContractError::VerificationProcessError)
                     }
-                    _ => return Err(ContractError::VerificationProcessError)
                 }
+                return Err(ContractError::VerificationProcessError)
             }
-            return Err(ContractError::VerificationProcessError)
-        }
-        GIVE_ME_FOOD_ROUTE_ID => {
-            let verification_result = parse_reply_execute_data(reply)?;
-            let verified: bool = from_json(&verification_result.data.unwrap())?;
-            let msg = PENDING_TRANSACTIONS.load(deps.storage, GIVE_ME_FOOD_ROUTE_ID)?;
-            if verified {
-                match msg {
-                    ExecuteMsg::GiveMeSomeFood(msg) => {
-                        return Ok(Response::new()
-                            .add_attribute("action", "give_me_some_food")
-                            .add_attribute("Food kind", msg.kind)
-                        )
+            GIVE_ME_GASOLINE_ROUTE_ID => {
+                let verification_result = parse_reply_execute_data(reply)?;
+                let verified: bool = from_json(&verification_result.data.unwrap())?;
+                let msg = self.pending_transactions.load(ctx.deps.storage, GIVE_ME_GASOLINE_ROUTE_ID)?;
+                if verified {
+                    match msg {
+                        ExecuteMsg::GiveMeSomeGasoline(msg) => {
+                            return Ok(Response::new()
+                                .add_attribute("action", "give_me_some_gasoline")
+                                .add_attribute("Amount of gasoline required", msg.amount)
+                            )
+                        }
+                        _ => return Err(ContractError::VerificationProcessError)
                     }
-                    _ => return Err(ContractError::VerificationProcessError)
                 }
+                return Err(ContractError::VerificationProcessError)
             }
-            return Err(ContractError::VerificationProcessError)
+            _ => return Err(ContractError::InvalidRouteId)
         }
-        GIVE_ME_GASOLINE_ROUTE_ID => {
-            let verification_result = parse_reply_execute_data(reply)?;
-            let verified: bool = from_json(&verification_result.data.unwrap())?;
-            let msg = PENDING_TRANSACTIONS.load(deps.storage, GIVE_ME_GASOLINE_ROUTE_ID)?;
-            if verified {
-                match msg {
-                    ExecuteMsg::GiveMeSomeGasoline(msg) => {
-                        return Ok(Response::new()
-                            .add_attribute("action", "give_me_some_gasoline")
-                            .add_attribute("Amount of gasoline required", msg.amount)
-                        )
-                    }
-                    _ => return Err(ContractError::VerificationProcessError)
-                }
-            }
-            return Err(ContractError::VerificationProcessError)
-        }
-        _ => return Err(ContractError::InvalidRouteId)
     }
 }
