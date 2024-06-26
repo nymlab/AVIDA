76b7b75c0ed1e706ee4020557e6b3e4ee0ba2546
lampkin-diet <lampkin.diet@gmail.com>
2024-06-26 20:50:59 +0200

    Fix ts style checks

--- avida-ts/scripts/gen-contract-types.mjs
+++ avida-ts/scripts/gen-contract-types.mjs
@@ -5,7 +5,7 @@
  * `avida/contracts`.
  **/
 
-import { default as codegen } from "@cosmwasm/ts-codegen";
+import codegen from "@cosmwasm/ts-codegen";
 import fs from "fs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
@@ -49,26 +49,25 @@ console.log("OUT_DIR", OUT_DIR);
 
 console.log("Generating TS files from JSON schema files...");
 
-{
-  await codegen.default({
-    contracts: CONTRACTS.map(({ path, name }) => ({
-      name,
-      dir: join(CONTRACT_DIR, path),
-    })),
-    outPath: OUT_DIR,
-    options: {
-      bundle: {
-        enabled: true,
-        bundleFile: "contracts.ts",
-      },
-      client: {
-        enabled: false,
-      },
-      messageBuilder: {
-        enabled: false,
-      },
+// @ts-ignore: Strange  module importing
+codegen.default({
+  contracts: CONTRACTS.map(({ path, name }) => ({
+    name,
+    dir: join(CONTRACT_DIR, path),
+  })),
+  outPath: OUT_DIR,
+  options: {
+    bundle: {
+      enabled: true,
+      bundleFile: "contracts.ts",
     },
+    client: {
+      enabled: false,
+    },
+    messageBuilder: {
+      enabled: false,
+    },
+  },               
+}).then(() => {
+    console.log("Contract ts generation completed successfully!");
   });
-}
-
-console.log("Contract ts generation completed successfully!");
