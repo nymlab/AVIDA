0d72fc3481de1e54d45c0ec8f3f8f65ef566f05e
bwty <whalelephant@users.noreply.github.com>
2024-06-27 17:00:52 +0800

    fix: use neverthrow

--- avida-ts/packages/avida-tx-utils/src/index.ts
+++ avida-ts/packages/avida-tx-utils/src/index.ts
@@ -3,6 +3,10 @@ export * from "./wallet";
 export * from "./exec";
 export * from "./sdjwt_utils";
 import * as base64js from "base64-js";
+import type {
+  CosmosTxV1beta1Fee as Fee,
+  CosmosBaseAbciV1beta1TxResponse as TxResponse,
+} from "cosmes/protobufs";
 
 // returns base64 encoded stringify json as `Binary` type in cosmwasm
 export function toWasmBinary<T extends object | string>(data: T): string {
@@ -13,3 +17,40 @@ export function toWasmBinary<T extends object | string>(data: T): string {
 
   return base64js.fromByteArray(encodedData);
 }
+
+import { type ResultAsync, fromPromise } from "neverthrow";
+import type { MnemonicWallet, UnsignedTx } from "cosmes/wallet";
+
+export interface TxUtilError {
+  EstimateFeeError: string;
+  BroadcastError: string;
+  PollTxError: string;
+}
+
+export const feePromise = (
+  storeTx: UnsignedTx,
+  wallet: MnemonicWallet,
+): ResultAsync<Fee, TxUtilError> => {
+  return fromPromise(wallet.estimateFee(storeTx), (e: unknown) => {
+    return { EstimateFeeError: e as string } as TxUtilError;
+  });
+};
+
+export const broadcastTxPromise = (
+  storeTx: UnsignedTx,
+  wallet: MnemonicWallet,
+  fee: Fee,
+): ResultAsync<string, TxUtilError> => {
+  return fromPromise(wallet.broadcastTx(storeTx, fee), (e: unknown) => {
+    return { BroadcastError: e as string } as TxUtilError;
+  });
+};
+
+export const pollTxPromise = (
+  txHash: string,
+  wallet: MnemonicWallet,
+): ResultAsync<TxResponse, TxUtilError> => {
+  return fromPromise(wallet.pollTx(txHash), (e: unknown) => {
+    return { PollTxError: e as string } as TxUtilError;
+  }).map((res) => res.txResponse as TxResponse);
+};
