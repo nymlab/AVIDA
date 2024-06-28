bb963166e22a216b3a800bf55c94ee5578ab9025
bwty <whalelephant@users.noreply.github.com>
2024-06-28 13:39:51 +0800

    refactor: example contract tests

--- contracts/avida_example/src/tests/exec_test.rs
+++ contracts/avida_example/src/tests/exec_test.rs
@@ -1,4 +1,3 @@
-use cosmwasm_std::Addr;
 use sylvia::multitest::App;
 
 use avida_common::traits::avida_verifier_trait::sv::mt::AvidaVerifierTraitProxy;
@@ -8,37 +7,34 @@ use crate::constants::GIVE_ME_DRINK_ROUTE_ID;
 use crate::contract::sv::mt::{CodeId as RestaurantCodeID, RestaurantContractProxy};
 use crate::tests::fixtures::setup_requirement;
 use crate::types::RegisterRequirement;
-use avida_test_utils::sdjwt::fixtures::issuer_jwk;
+use avida_test_utils::sdjwt::fixtures::{issuer_jwk, MAX_PRESENTATION_LEN, OWNER_ADDR};
 
 #[test]
 fn register_requirement() {
     let app = App::default();
-    let owner = Addr::unchecked("owner"); // "owner";
-                                          // Storages for contracts
+    // Storages for contracts
     let code_id_verifier = VerifierCodeID::store_code(&app);
     let code_id_restaurant = RestaurantCodeID::store_code(&app);
 
     // Instantiate contracts
-    let max_presentation_len = 3000usize;
     let contract_verifier = code_id_verifier
-        .instantiate(max_presentation_len, vec![])
+        .instantiate(MAX_PRESENTATION_LEN, vec![])
         .with_label("Verifier")
-        .call(owner.as_str())
+        .call(OWNER_ADDR)
         .unwrap();
 
-    let addr = owner.as_str();
     let contract_restaurant = code_id_restaurant
         .instantiate(contract_verifier.contract_addr.to_string())
         .with_label("Restaurant")
-        .call(addr)
+        .call(OWNER_ADDR)
         .unwrap();
     // Setup requirement
-    let fx_route_verification_req = setup_requirement();
+    let fx_route_verification_req = setup_requirement("drink");
     let _a = contract_restaurant
         .register_requirement(RegisterRequirement::Drink {
             requirements: fx_route_verification_req.clone(),
         })
-        .call(owner.as_str())
+        .call(OWNER_ADDR)
         .unwrap();
     let registered_routes = contract_verifier
         .get_routes(contract_restaurant.contract_addr.to_string())
--- contracts/avida_example/src/tests/fixtures.rs
+++ contracts/avida_example/src/tests/fixtures.rs
@@ -5,8 +5,14 @@ use avida_test_utils::sdjwt::fixtures::{
     PresentationVerificationType, RouteVerificationRequirementsType,
 };
 
-pub fn create_presentation() -> String {
-    let claims = claims("Alice", 30, true, 2021, None);
+use crate::contract::{sv::mt::CodeId as RestaurantCodeID, RestaurantContract};
+use avida_sdjwt_verifier::contract::sv::mt::CodeId as VerifierCodeID;
+use avida_test_utils::sdjwt::fixtures::{MAX_PRESENTATION_LEN, OWNER_ADDR as caller};
+use sylvia::cw_multi_test::App as MtApp;
+use sylvia::multitest::{App, Proxy};
+
+pub fn create_presentation(age: u8) -> String {
+    let claims = claims("Alice", age, true, 2021, None);
     make_presentation(claims, PresentationVerificationType::Success)
 }
 
@@ -20,12 +26,16 @@ pub fn create_presentation_with_exp(expired: bool) -> String {
     make_presentation(claims, PresentationVerificationType::Success)
 }
 
-pub fn setup_requirement() -> RouteVerificationRequirements {
-    // Add only 1 criterion - age greater than 18
-    let presentation_req: PresentationReq = vec![(
-        "age".to_string(),
-        Criterion::Number(18, MathsOperator::GreaterThan),
-    )];
+pub fn setup_requirement(order: &str) -> RouteVerificationRequirements {
+    // Add only 1 criterion - age greater than 18 for drink, and none for food
+    let presentation_req: PresentationReq = match order {
+        "drink" => vec![(
+            "age".to_string(),
+            Criterion::Number(18, MathsOperator::GreaterThan),
+        )],
+        "food" => vec![],
+        _ => vec![],
+    };
     make_route_verification_requirements(
         presentation_req,
         RouteVerificationRequirementsType::Supported,
@@ -33,7 +43,7 @@ pub fn setup_requirement() -> RouteVerificationRequirements {
 }
 
 pub fn setup_requirement_with_expiration() -> RouteVerificationRequirements {
-    // Add only 1 criterion - age greater than 18
+    // Add 2 criterion - age greater than 18, and expiration
     let presentation_req: PresentationReq = vec![
         (
             "age".to_string(),
@@ -46,4 +56,23 @@ pub fn setup_requirement_with_expiration() -> RouteVerificationRequirements {
         presentation_req,
         RouteVerificationRequirementsType::Supported,
     )
+}
+
+pub fn instantiate_contracts(app: &App<MtApp>) -> Proxy<'_, MtApp, RestaurantContract<'_>> {
+    // Storages for contracts
+    let code_id_verifier = VerifierCodeID::store_code(app);
+    let code_id_restaurant = RestaurantCodeID::store_code(app);
+
+    // Instantiate contracts
+    let contract_verifier = code_id_verifier
+        .instantiate(MAX_PRESENTATION_LEN, vec![])
+        .with_label("Verifier")
+        .call(caller)
+        .unwrap();
+
+    code_id_restaurant
+        .instantiate(contract_verifier.contract_addr.to_string())
+        .with_label("Restaurant")
+        .call(caller)
+        .unwrap()
 }
