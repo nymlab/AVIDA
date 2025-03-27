3dcc9f4877629d7c8eb969d0a9c830d64b6fc460
bwty <whalelephant@users.noreply.github.com>
2025-03-27 19:05:34 +0100

    fix: revert build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,9 @@ OS="`uname -p`"
 echo "Building for arch = $OS"
 
 if [ $OS = 'arm' ]; then
-  OPTIMIZER="cosmwasm/optimizer-arm64:0.16.1"
+  OPTIMIZER="ghcr.io/nymlab/optimizer-arm64:0.15.1-clang"
 elif [ $OS = 'x86_64' ]; then
-  OPTIMIZER="cosmwasm/optimizer:0.16.1"
+  OPTIMIZER="ghcr.io/nymlab/rust-optimizer:39077c998b881011f3db2cb5c1dbe9904e6be8f1"
 else
   return 1
 fi
