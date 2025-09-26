31b3611f1c4e450658c81a2712e47e0e5b535575
bwty <whalelephant@users.noreply.github.com>
2025-09-26 10:47:42 +0100

    update optimizer

--- scripts/build-optimise.sh
+++ scripts/build-optimise.sh
@@ -7,7 +7,8 @@ echo "Building for arch = $OS"
 if [ $OS = 'arm64' ]; then
   OPTIMIZER="ghcr.io/nymlab/optimizer-arm64:0.17.0-clang"
 elif [ $OS = 'x86_64' ]; then
-  OPTIMIZER="cosmwasm/optimizer:0.17.0"
+  # This will need to be updated
+  OPTIMIZER="ghcr.io/nymlab/rust-optimizer:39077c998b881011f3db2cb5c1dbe9904e6be8f1"
 else
   return 1
 fi
@@ -16,10 +17,3 @@ docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   $OPTIMIZER
-
-
-# for file in ./artifacts/*; do
-#   BASENAME=$(basename "$file")
-#   NEWNAME=$(echo "$BASENAME" | sed 's/-aarch64//')
-#   mv "$file" "./artifacts/$NEWNAME"
-# done
